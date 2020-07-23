cd3e6f0562627f982c24280101c71da1fdf1024c
trebesh <50559259+trebesh@users.noreply.github.com>
2020-07-23 13:10:44 +1200

    Create VNS.java

---
+++ VNS.java
@@ -0,0 +1,20 @@
+/*
+  Main Class for calling functions to Implement Variable Neighbourhood Search
+  
+  ConnorFergusson_1299038_HannahTrebes_1306378
+*/
+
+ 
+public class VNS{
+	public static void main(String [] args)
+	{
+    public ArrayList[][] boxes = new ArrayList[][2];
+    public ArrayList[][] pos = new ArrayList[][2];
+    
+    getData();
+    rotateBoxes();
+    initialSolution();
+    reuslt();
+    
+  }
+}
