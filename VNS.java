f069cde809e301256876d1808cba41fede564a00
CelestialWrath <56210999+CelestialWrath@users.noreply.github.com>
2020-07-27 11:54:26 +1200

    Update VNS.java

--- VNS.java
+++ VNS.java
@@ -71,4 +71,17 @@ public static final int width = 15;
         result();
         System.out.println(Integer.toString(totalHeight));
     }
+	
+	//a method that writes the position of of each box to the console
+    public static void result()
+    {
+        //stores the size of the position array
+        int size = InitialSolution.pos.size();
+        //loops through the position array
+        for(int i = 0; i<size; i++)
+        {
+            //writes each box to the console on separate lines
+            System.out.println("Box " + Integer.toString(i + 1) + " is at position: " + InitialSolution.pos.get(i).get(0) + ", " + InitialSolution.pos.get(i).get(1));
+        }
+    }
 }
