242494844b2820bceb994c05d912ca0729e91da8
CelestialWrath <56210999+CelestialWrath@users.noreply.github.com>
2020-07-27 11:56:50 +1200

    Update VNS.java

--- VNS.java
+++ VNS.java
@@ -8,16 +8,79 @@
 public class VNS{
 	public static void main(String [] args)
 	{
-    	public ArrayList[][] boxes = new ArrayList[][2];
-    public ArrayList[][] pos = new ArrayList[][2];
+    	public static ArrayList<Integer> box;
+    //List of the items
+    public static ArrayList<ArrayList<Integer>> boxes = new ArrayList<ArrayList<Integer>>();
+    //Buffered Reader - read line by line
 public static final int width = 15;
     
     getData();
-    rotateBoxes();
     initialSolution();
     reuslt();
     
   }
+	
+	// VARIABLES -------------------------------------------------------------------------------------------------------
+    //An item
+    
+    public static BufferedReader BR;
+    //The line
+    public static String line;
+    //Valid file extension
+    public static String csv = "csv";
+
+    // MAIN ------------------------------------------------------------------------------------------------------------
+    public static void getData()
+    {
+        //Check that a file path is valid
+        if (args.length == 0){
+            System.out.println("Error: File Path needed as argument");
+            return;
+        }
+        else if(!csv.equals(args[0].substring(args[0].length() - 3))){
+            System.out.println("ERROR: File not in csv format");
+            return;
+        }
+        String filePath = args[0];
+
+        getData(filePath);
+    }
+
+    // GETDATA ---------------------------------------------------------------------------------------------------------
+    // Reads the data in from the provided csv file
+    public static void getData(String filePath){
+        try{
+            BR = new BufferedReader(new FileReader(filePath));
+            //Until the end of the file is reached
+            while ((line = BR.readLine()) != null){
+                box = new ArrayList<Integer>();
+                //Split the line into the list of items
+                String[] stringDim = line.split(",");
+
+                //Rotate the box 90 degrees if it's taller than it is wide
+                if(Integer.parseInt(stringDim[0]) < Integer.parseInt(stringDim[1])){
+                    box.add(Integer.parseInt(stringDim[1]));
+                    box.add(Integer.parseInt(stringDim[0]));
+                }
+                else{
+                    box.add(Integer.parseInt(stringDim[0]));
+                    box.add(Integer.parseInt(stringDim[1]));
+                }
+
+                // Add the box to the list of all boxes
+                boxes.add(box);
+            }
+            //Testing message
+            for(int i = 0; i < boxes.size(); i++){
+                System.out.println(boxes.get(i).get(0)+ " " + boxes.get(i).get(1));
+            }
+        }catch (IOException e){
+            System.out.println("IO ERROR in GETDATA" + e.getMessage());
+        }catch (Exception e){
+            System.out.println("ERROR in GETDATA" + e.getMessage());
+        }
+        return;
+    }
 	
 	//declares a variable for the width of the sheet
    
@@ -32,10 +95,10 @@ public static final int width = 15;
         int nextHeight = 0;
 
         //loop through the list of boxes
-        for(int i = 0; i<boxes.size(); i++)
+        for(int i = 0; i<box.size(); i++)
         {
             //get the nextbox in the list
-            ArrayList<Integer> current = boxes.get(i);
+            ArrayList<Integer> current = box.get(i);
             //get its height and width
             nextHeight = current.get(1);
             nextWidth = current.get(0);
@@ -76,12 +139,12 @@ public static final int width = 15;
     public static void result()
     {
         //stores the size of the position array
-        int size = InitialSolution.pos.size();
+        int size = pos.size();
         //loops through the position array
         for(int i = 0; i<size; i++)
         {
             //writes each box to the console on separate lines
-            System.out.println("Box " + Integer.toString(i + 1) + " is at position: " + InitialSolution.pos.get(i).get(0) + ", " + InitialSolution.pos.get(i).get(1));
+            System.out.println("Box " + Integer.toString(i + 1) + " is at position: " + pos.get(i).get(0) + ", " + pos.get(i).get(1));
         }
     }
 }
