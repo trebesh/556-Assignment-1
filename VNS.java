e63952d6412d0a8ca20e6245832f79e626c39fd4
CelestialWrath <56210999+CelestialWrath@users.noreply.github.com>
2020-07-27 12:07:42 +1200

    Update VNS.java

--- VNS.java
+++ VNS.java
@@ -3,6 +3,11 @@
   
   ConnorFergusson_1299038_HannahTrebes_1306378
 */
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
  
 public class VNS{
